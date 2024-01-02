0a0049769d7d9a2f650d19e859740f3b6dacfa10
Ricardo Avelino <rick58@ymail.com>
2024-01-02 14:40:42 -0300

    feat: add tratamento de erro

--- hello-world01/app/Controllers/Http/DrinksController.ts
+++ hello-world01/app/Controllers/Http/DrinksController.ts
@@ -10,42 +10,73 @@ export default class DrinksController {
   constructor(private readonly drinkRepository: DrinkRepositoryInterface) {}
   public async index({ request }: HttpContextContract) {
     const { name, category, page, rowsPerPage } = request.qs()
-
     return this.drinkRepository.getAll({ name, category }, { page, rowsPerPage })
   }
 
-  public async store({ request }: HttpContextContract) {
-    await request.validate(StoreValidator)
-    const drink: IDrink = request.only(['name', 'instructions', 'category'])
-    const image = request.file('image')
-
-    if (image) {
-      const imageName = `${randomUUID()}.${image.extname}`
-      await image?.moveToDisk(
-        'images',
-        {
-          name: imageName,
-        },
-        's3'
-      )
-
-      drink.image = `${process.env.S3_BUCKET_URL}${imageName}`
+  public async store({ request, response }: HttpContextContract) {
+    try {
+      await request.validate(StoreValidator)
+      const drink: IDrink = request.only(['name', 'instructions', 'category'])
+      const image = request.file('image')
+      if (image) {
+        const imageName = `${randomUUID()}.${image.extname}`
+        await image?.moveToDisk('images', { name: imageName }, 's3')
+        drink.image = `${process.env.S3_BUCKET_URL}${imageName}`
+      }
+      return this.drinkRepository.create(drink)
+    } catch (error) {
+      if (error?.status && error?.message) {
+        return response.status(error?.status).send({ error: error.message.toString() })
+      } else {
+        return response.status(500).send({ error: 'Internal server error' })
+      }
     }
-
-    return this.drinkRepository.create(drink)
   }
 
-  public async show({ params }: HttpContextContract) {
-    return this.drinkRepository.getById(params.id)
+  public async show({ params, response }: HttpContextContract) {
+    try {
+      if (isNaN(Number(params.id))) {
+        return response.status(400).send({ error: 'Invalid id' })
+      }
+      return this.drinkRepository.getById(params.id)
+    } catch (error) {
+      if (error?.status && error?.message) {
+        return response.status(error?.status).send({ error: error.message.toString() })
+      } else {
+        return response.status(500).send({ error: 'Internal server error' })
+      }
+    }
   }
 
-  public async update({ request, params }: HttpContextContract) {
-    await request.validate(UpdateValidator)
-    const updatedDrink: IDrink = request.only(['name', 'instructions', 'image', 'category'])
-    return this.drinkRepository.update(params.id, updatedDrink)
+  public async update({ request, params, response }: HttpContextContract) {
+    try {
+      if (isNaN(Number(params.id))) {
+        return response.status(400).send({ error: 'Invalid id' })
+      }
+      await request.validate(UpdateValidator)
+      const updatedDrink: IDrink = request.only(['name', 'instructions', 'image', 'category'])
+      return this.drinkRepository.update(params.id, updatedDrink)
+    } catch (error) {
+      if (error?.status && error?.message) {
+        return response.status(error?.status).send({ error: error.message.toString() })
+      } else {
+        return response.status(500).send({ error: 'Internal server error' })
+      }
+    }
   }
 
-  public async destroy({ params }: HttpContextContract) {
-    return await this.drinkRepository.remove(params.id)
+  public async destroy({ params, response }: HttpContextContract) {
+    try {
+      if (isNaN(Number(params.id))) {
+        return response.status(400).send({ error: 'Invalid id' })
+      }
+      return await this.drinkRepository.remove(params.id)
+    } catch (error) {
+      if (error?.status && error?.message) {
+        return response.status(error?.status).send({ error: error.message.toString() })
+      } else {
+        return response.status(500).send({ error: 'Internal server error' })
+      }
+    }
   }
 }
